9574b5d8666e9a1004e73d1e9e81d4575b430d7b
aniskehila <aniskehila22@gmail.com>
2023-08-27 16:03:32 +0100

    add files

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  let filePath = "." + req.url;
+
+  if (filePath === "./") {
+    filePath = "./home";
+  }
+  filePath = path.join(__dirname, `${filePath}.html`);
+  console.log(filePath);
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        filePath = path.join(__dirname, "404.html");
+        fs.readFile(filePath, (err, content) => {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end(content, "utf-8");
+        });
+      } else {
+        res.writeHead(500);
+        res.end("Internal Server Error");
+      }
+    } else {
+      // Successful response
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content, "utf-8");
+    }
+  });
+});
+
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
